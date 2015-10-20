b821fd49fb7d6fa5a757b6072e3c4da8bd94b88c
Jack McKenzie <jackmckenzie.home@gmail.com>
2015-10-20 05:34:04 +0100

    Improved code structure and simplified the flow

--- src/main/java/com/cor/cep/event/TemperatureEvent.java
+++ src/main/java/com/cor/cep/event/TemperatureEvent.java
@@ -2,51 +2,34 @@ package com.cor.cep.event;
 
 import java.util.Date;
 
-/**
- * Immutable Temperature Event class. The process control system creates these events. The
- * TemperatureEventHandler picks these up and processes them.
- */
 public class TemperatureEvent {
-
-    /** Temperature in Celcius. */
     private int temperature;
     
-    /** Time temerature reading was taken. */
     private Date timeOfReading;
     
-    /**
-     * Single value constructor.
-     * @param value Temperature in Celsius.
-     */
-    /**
-     * Temeratur constructor.
-     * @param temperature Temperature in Celsius
-     * @param timeOfReading Time of Reading
-     */
     public TemperatureEvent(int temperature, Date timeOfReading) {
         this.temperature = temperature;
         this.timeOfReading = timeOfReading;
     }
 
-    /**
-     * Get the Temperature.
-     * @return Temperature in Celsius
-     */
     public int getTemperature() {
         return temperature;
     }
-       
-    /**
-     * Get time Temperature reading was taken.
-     * @return Time of Reading
-     */
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+
     public Date getTimeOfReading() {
         return timeOfReading;
+    }
+
+    public void setTimeOfReading(Date timeOfReading) {
+        this.timeOfReading = timeOfReading;
     }
 
     @Override
     public String toString() {
         return "TemperatureEvent [" + temperature + "C]";
     }
-
 }
