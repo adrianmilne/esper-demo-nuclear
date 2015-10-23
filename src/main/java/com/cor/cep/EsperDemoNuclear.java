1d9f83ce99aec787c1f59bb93389052421f6c774
Jack McKenzie <jackmckenzie.home@gmail.com>
2015-10-23 16:18:12 +0100

    Improved cohesion

--- src/main/java/com/cor/cep/EsperDemoNuclear.java
+++ src/main/java/com/cor/cep/EsperDemoNuclear.java
@@ -4,12 +4,11 @@ import com.cor.cep.action.TemperatureCriticalAction;
 import com.cor.cep.action.TemperatureMonitorAction;
 import com.cor.cep.action.TemperatureWarningAction;
 import com.cor.cep.event.TemperatureEvent;
-import com.cor.cep.statement.TemperatureCriticalStatement;
-import com.cor.cep.statement.TemperatureMonitorStatement;
-import com.cor.cep.statement.TemperatureWarningStatement;
+import com.cor.cep.statement.TemperatureStatement;
 import com.espertech.esper.client.Configuration;
 import com.espertech.esper.client.EPServiceProvider;
 import com.espertech.esper.client.EPServiceProviderManager;
+import com.espertech.esper.client.EPStatement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,7 +19,7 @@ import java.util.Random;
  * Entry point for the Demo. Run this from your IDE, or from the command line using 'mvn exec:java'.
  */
 public class EsperDemoNuclear {
-    private static Logger LOG = LoggerFactory.getLogger(EsperDemoNuclear.class);
+    private static final Logger logger = LoggerFactory.getLogger(EsperDemoNuclear.class);
 
     public static void main(final String[] args) {
         StringBuilder sb = new StringBuilder();
@@ -29,34 +28,47 @@ public class EsperDemoNuclear {
         sb.append("\n* PLEASE WAIT - TEMPERATURES ARE RANDOM SO MAY TAKE");
         sb.append("\n* A WHILE TO SEE WARNING AND CRITICAL EVENTS!");
         sb.append("\n************************************************************\n");
-        LOG.debug(sb.toString());
+        logger.debug(sb.toString());
 
-        long numTemperatureEvents = 1000;
+        long demoMaxEventCount = 1000;
         if (args.length != 1) {
-            LOG.debug("No override of number of events detected - defaulting to " + numTemperatureEvents + " events.");
+            logger.debug("No override of number of events detected - defaulting to " + demoMaxEventCount + " events.");
         } else {
-            numTemperatureEvents = Long.valueOf(args[0]);
+            demoMaxEventCount = Long.valueOf(args[0]);
         }
 
-        LOG.debug("Initializing the Complex Event Processing Service ..");
         Configuration config = new Configuration();
         config.addEventTypeAutoName("com.cor.cep.event");
         EPServiceProvider epService = EPServiceProviderManager.getDefaultProvider(config);
 
-        LOG.debug("Registering Event Processing statements and actions to be triggered on matching data");
-        TemperatureCriticalStatement.createAndSubscribe(epService, new TemperatureCriticalAction());
-        TemperatureWarningStatement.createAndSubscribe(epService, new TemperatureWarningAction());
-        TemperatureMonitorStatement.createAndSubscribe(epService, new TemperatureMonitorAction());
+        // Register event pattern queries in the CEP and actions to be triggered on a match
+        EPStatement ep = epService.getEPAdministrator().createEPL(TemperatureStatement.QUERY_CRITICAL_TEMPERATURE);
+        ep.setSubscriber(new TemperatureCriticalAction());
 
-        for (int count = 0; count < numTemperatureEvents; count++) {
+        ep = epService.getEPAdministrator().createEPL(TemperatureStatement.QUERY_WARNING_TEMPERATURE);
+        ep.setSubscriber(new TemperatureWarningAction());
+
+        ep = epService.getEPAdministrator().createEPL(TemperatureStatement.QUERY_TEMPERATURE_MONITOR);
+        ep.setSubscriber(new TemperatureMonitorAction());
+
+        // create an event stream and send events to the CEP
+        for (int count = 0; count < demoMaxEventCount; count++) {
             TemperatureEvent temperatureEvent = new TemperatureEvent(new Random().nextInt(500), new Date());
-            LOG.debug(temperatureEvent.toString());
+            logger.debug(temperatureEvent.toString());
+
+            // send event to the CEP [which also makes the CEP run all queries and trigger respective actions]
+            // A time window [i.e. 10s or as defined] of all events is queried in the CEP and when event patterns
+            // match any above query the CEP triggers its subscribed action
             epService.getEPRuntime().sendEvent(temperatureEvent);
+
+            // This is not required. For illustration, add some delay (sleep)
             try {
                 Thread.sleep(200);
             } catch (InterruptedException e) {
-                LOG.error("Thread Interrupted", e);
+                logger.error("Thread Interrupted", e);
             }
         }
+
+        logger.debug("Finished");
     }
 }
